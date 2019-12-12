2522958b975c2f2f17db4795e59758b37fa35fb5
Дмитрий Астахов <teamvortex@yandex.ru>
2019-12-12 20:15:08 +0300

    FINAL

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,11 +4,33 @@
 #include "header.hpp"
 
 TEST(Example, EmptyTest) {
-int* a = new int;
+int *a = new int;
 *a = 7;
+int *b = new int;
+*b = 8;
 
 SharedPtr<int> s1(a);
 EXPECT_EQ(s1.use_count(), 1);
+EXPECT_EQ(s1.get(), a);
+
+SharedPtr<int> s2(a);
+SharedPtr<int> s3(b);
+
+EXPECT_EQ(*s1, *a);
+s1.my_swap(s2);
+EXPECT_EQ(s1, s2);
+
+s1.reset();
+EXPECT_EQ(s1.use_count(), 0);
+
+s1 = s2;
+EXPECT_EQ(s1, s2);
+
+s1.reset(b);
+EXPECT_EQ(s1.use_count(), s3.use_count());
+
+SharedPtr<int> s3(s2);
+EXPECT_EQ(s3, s2);
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
